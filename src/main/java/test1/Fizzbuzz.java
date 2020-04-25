b4b2fd1cf20d1e41255cbf3015d10e0d76f299af
lis05 <lishuang_read@126.com>
2020-04-25 09:27:08 +0800

    add something

---
+++ src/main/java/test1/Fizzbuzz.java
@@ -0,0 +1,11 @@
+package test1;
+
+/**
+ * @author ：ls05
+ * @date ：Created in 2020/4/25 9:23
+ */
+public class Fizzbuzz {
+    public static String printFizzBuzzNum(int count) {
+        return String.valueOf(count);
+    }
+}
---
+++ src/test/java/FizzbuzzTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import test1.Fizzbuzz;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+/**
+ * @author ：ls05
+ * @date ：Created in 2020/4/25 9:18
+ */
+public class FizzbuzzTest {
+    @Test
+    public void should_get_num_when_no_del_by_5_or_3(){
+        assertThat(Fizzbuzz.printFizzBuzzNum(1)).isEqualTo("1");
+    }
+}
