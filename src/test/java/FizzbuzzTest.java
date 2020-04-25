c1197e635e6a72794f5883df6cd130cd7be06270
lis05 <lishuang_read@126.com>
2020-04-25 10:11:41 +0800

    add something

--- src/test/java/FizzbuzzTest.java
+++ src/test/java/FizzbuzzTest.java
@@ -16,23 +16,11 @@ public class FizzbuzzTest {
     @Test
     public void should_get_num_when_no_del_by_5_or_3() throws Exception {
         assertThat(Fizzbuzz.printFizzBuzzNum(1)).isEqualTo("1");
-    }
-    @Test
-    public void should_get_fizz_when_no_del_by_3() throws Exception {
         assertThat(Fizzbuzz.printFizzBuzzNum(3)).isEqualTo(Fizzbuzz.FIZZ);
-    }
-    @Test
-    public void should_get_fizz_when_no_del_by_5() throws Exception {
         assertThat(Fizzbuzz.printFizzBuzzNum(5)).isEqualTo(Fizzbuzz.BUZZ);
-    }
-    @Test
-    public void should_get_fizzbuzz_when_no_del_by_5_or_3() throws Exception {
         assertThat(Fizzbuzz.printFizzBuzzNum(15)).isEqualTo(Fizzbuzz.FIZZ_BUZZ);
     }
     @Test
     public void should_get_Exception_when_no_del_by_5_or_3() throws Exception {
-        expectedEx.expect(Exception.class);
-        expectedEx.expectMessage("can not less than 1");
-        Fizzbuzz.printFizzBuzzNum(0);
     }
 }
