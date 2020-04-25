2987b7d0431d84847fa2ecc72d6e1b693dc93847
lis05 <lishuang_read@126.com>
2020-04-25 09:59:00 +0800

    add something

--- src/main/java/test1/Fizzbuzz.java
+++ src/main/java/test1/Fizzbuzz.java
@@ -5,10 +5,28 @@ package test1;
  * @date ：Created in 2020/4/25 9:23
  */
 public class Fizzbuzz {
-    public static String printFizzBuzzNum(int count) {
-        if (count % 3 == 0) {
-            return "fizz";
+
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String FIZZ_BUZZ = "FizzBuzz";
+
+    public static String printFizzBuzzNum(int count) throws Exception {
+        if (count < 1) {
+            throw new Exception("can not less than 1");
+        }
+        if (canDevByANum(count, 3) && (canDevByANum(count, 5))) {
+            return FIZZ_BUZZ;
+        }
+        if (canDevByANum(count, 3)) {
+            return FIZZ;
+        }
+        if (canDevByANum(count, 5)) {
+            return BUZZ;
         }
         return String.valueOf(count);
+    }
+
+    private static boolean canDevByANum(int count, int i) {
+        return count % i == 0;
     }
 }
--- src/test/java/FizzbuzzTest.java
+++ src/test/java/FizzbuzzTest.java
@@ -1,4 +1,6 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import test1.Fizzbuzz;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,12 +11,28 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
  * @date ：Created in 2020/4/25 9:18
  */
 public class FizzbuzzTest {
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
     @Test
-    public void should_get_num_when_no_del_by_5_or_3(){
+    public void should_get_num_when_no_del_by_5_or_3() throws Exception {
         assertThat(Fizzbuzz.printFizzBuzzNum(1)).isEqualTo("1");
     }
     @Test
-    public void should_get_fizz_when_no_del_by_3(){
-        assertThat(Fizzbuzz.printFizzBuzzNum(3)).isEqualTo("fizz");
+    public void should_get_fizz_when_no_del_by_3() throws Exception {
+        assertThat(Fizzbuzz.printFizzBuzzNum(3)).isEqualTo(Fizzbuzz.FIZZ);
+    }
+    @Test
+    public void should_get_fizz_when_no_del_by_5() throws Exception {
+        assertThat(Fizzbuzz.printFizzBuzzNum(5)).isEqualTo(Fizzbuzz.BUZZ);
+    }
+    @Test
+    public void should_get_fizzbuzz_when_no_del_by_5_or_3() throws Exception {
+        assertThat(Fizzbuzz.printFizzBuzzNum(15)).isEqualTo(Fizzbuzz.FIZZ_BUZZ);
+    }
+    @Test
+    public void should_get_Exception_when_no_del_by_5_or_3() throws Exception {
+        expectedEx.expect(Exception.class);
+        expectedEx.expectMessage("can not less than 1");
+        Fizzbuzz.printFizzBuzzNum(0);
     }
 }
