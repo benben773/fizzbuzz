14e284c8b3c23c872be71992432dcbdcfa3d069c
lis05 <lishuang_read@126.com>
2020-04-27 23:06:10 +0800

    fail test

---
+++ src/main/java/test1/FizzBuzz1.java
@@ -0,0 +1,7 @@
+package test1;
+
+public class FizzBuzz1 {
+    public String valueOf(int count) {
+        return null;
+    }
+}
---
+++ src/test/java/FizzBuzz1Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import test1.FizzBuzz1;
+
+import static org.junit.Assert.assertEquals;
+
+public class FizzBuzz1Test {
+    @Test
+    public void fizzBuzzTest() {
+        FizzBuzz1 fizzBuzz1 = new FizzBuzz1();
+        assertEquals(fizzBuzz1.valueOf(1),"1");
+    }
+}
